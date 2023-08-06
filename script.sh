62f6f94d9fdfc911ed03e09b89a626c89e2d238e
suhailroushan13 <suhailroushan13@gmail.com>
2023-08-06 22:13:38 +0530

    Updated

---
+++ script.sh
@@ -0,0 +1,14 @@
+GREEN='\033[0;32m'
+RESET='\033[0m'
+URL="https://github.com/suhailroushan13"
+TEXT="Happy Hacking From Suhail Roushan🚀"
+
+cd ~bin
+sudo touch npm-mern-template
+sudo chmod 777 npm-mern-template
+url="https://gist.githubusercontent.com/suhailroushan13/d77137da5e0fd5062a93c80f2bc0a3ff/raw/a10d2c1c3819c9280edf9de86c6fde474ec88fd2/script.sh"
+curl "$url" -o "npm-mern-template"
+
+
+echo -e "\nJust Type ${GREEN}npm-mern-template${RESET} in your ${GREEN}/home/user/${RESET} directory to Get Started With SetUp ✅\n"
+echo -e "${GREEN}\e]8;;${URL}\a${TEXT}\e]8;;\a${RESET}"
